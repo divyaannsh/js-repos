a1878b67aed4b06464281463da48a8fd8858745b
Divyansh <divyanshsrivastav72@gmail.com>
2023-08-24 14:42:46 +0530

    repo

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId =1222
+
+let accountEmail = "hitesh@gmail.com"
+
+var accountPassword ="1134"
+
+accountCity = "jaipur"
+
+//accountId = 2
+ console.log(accountId);
+
+
+ accountEmail ="jenekk"
+ accountPassword ="ejees"
+
+ accountCity ="jddd"
+
+ console.log(accountId)
+ console.table([accountId,accountEmail,accountCity,accountPassword]);
---
+++ 02_datatypes/test.js
@@ -0,0 +1,15 @@
+"use stricr"; //treat all js code as newer versionnnn
+
+
+console.log(3+3);
+
+
+let name ="hitesh"
+let age =18
+//number=> 2 to power 53
+//bigint
+//str ing => ""
+//boolean => true/false
+//null => standalone value
+
+console.log(typeof age);
