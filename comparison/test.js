aaa20f5ed12e3ae4a86a6312d35ff312e4c2700f
Divyansh <divyanshsrivastav72@gmail.com>
2023-08-28 15:00:17 +0530

    update

---
+++ comparison/test.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+
+console.log(null>0);
+console.log(null>=0);
+//===4
+console.log("2"==2);
+
+//datatypes interview 
+//primitive datatypes
+//7 types : string,number , boolean, null , undefined,symbol, bigInt
+
+const score = 100 
+const scoreValue =10.3
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id== anotherId);
+//refrence type(non primitive)
+      //arrray, objects,funtion   
+
+      
---
+++ conversion/test.js
@@ -0,0 +1,23 @@
+//let score =33
+//console.log(typeof score);
+
+let value=3
+let negvalue =-value
+//console.log(negvalue);
+//console.log(2+2);
+
+let str1 =" hello "
+let str2 =" divyansh "
+let str4= str1 + str2
+//console.log(str4);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
