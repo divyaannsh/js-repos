a1f983b9ab6dc27d0bc561b98a83ad4c84b01e86
Divyansh <divyanshsrivastav72@gmail.com>
2023-09-14 18:32:08 +0530

    ents1/2

---
+++ events/test.js
@@ -0,0 +1,8 @@
+document.getElementById('images').addEventListener('click',function(e){
+    console.log("clicked inside the ul");
+},false)
+
+
+document.getElementById('images').addEventListener('click',function(e){
+    console.log("owl clicked");
+},false)
