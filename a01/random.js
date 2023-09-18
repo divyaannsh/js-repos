3478ed5f78ef50b14bc144a23684a3f0582d7ccd
Divyansh <divyanshsrivastav72@gmail.com>
2023-09-18 21:42:51 +0530

    t

---
+++ a01/random.js
@@ -0,0 +1,20 @@
+//generate a random color
+
+const randomColor =function (){
+    const hex ='0123456789ABCDEF';
+    let color = '#';
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+
+
+    }
+    return color;
+};
+ const startChangingColor
+const startChangingColor = function(){
+    intervalid= set INterva; (changingbgColor,2000);
+}
+
+document.querySelector("#start").addEventListener('click',startChangingColor)
+
+document.querySelector("#stdp").addEventListener('click',startChangingColor)
