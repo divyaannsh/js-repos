614867862b3fe27e0a1c9c0633971db399ca27d4
Divyansh <divyanshsrivastav72@gmail.com>
2023-11-29 20:03:54 +0530

    Update test.js

--- events/test.js
+++ events/test.js
@@ -6,3 +6,6 @@ document.getElementById('images').addEventListener('click',function(e){
 document.getElementById('images').addEventListener('click',function(e){
     console.log("owl clicked");
 },false)
+
+const cart =["shoes", "panta" , " kurta" ]
+ 
