1f38c44a718f9d0449772ba80c09d44b1ae84fe5
Divyansh <divyanshsrivastav72@gmail.com>
2023-12-29 23:38:54 +0530

    ASYNC

--- Revision/app.js
+++ Revision/app.js
@@ -1,4 +1,11 @@
-// variables an constant
-score = 0
-lives =3
- 
+// generate a Random backgroundColor
+
+const randomColor = function(){
+    const hex ="0123456789ABCDEF"
+    let color ='#'
+    for(let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+       return color
+    };
+    console.log(randomColor);
